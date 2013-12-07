fd6a06af0c1daf60075ff05bf18cf2475254f526
Joachim De Beule <joachim.de.beule@gmail.com>
2013-12-07 16:20:57 +0100

    initial commit

---
+++ src/clj/kraken/ml/features.clj
@@ -0,0 +1,43 @@
+(ns kraken.ml.features
+  (:use [kraken.model]
+        [kraken.elastic])
+  (:require [clj-time.core :as tcore]
+            [clj-time.format :as tformat]
+            [clj-time.coerce :as tcoerce]))
+
+
+;;; Some helper functions to extract features from elastic (wip)
+
+
+;; all ticks from the last half our in descending order:
+(ticks :query (daterange-filter :time (tcore/ago (tcore/minutes 30)))
+       :sort [{:time {:order "desc"}}])
+
+;; all XLTCZEUR ticks from the last half our in descending order:
+(ticks :query (match-query :asset "XLTCZEUR")
+       :sort [{:time {:order "desc"}}])
+
+;; last XLTCZEUR tick:
+(ticks :query (filtered-query :query (match-query :asset "XLTCZEUR")
+                              :filter (daterange-filter :time (tcore/ago (tcore/minutes 30))))
+       :sort [{:time {:order "desc"}}]
+       :size 1)
+
+;; first XLTCZEUR tick
+(ticks :query (match-query :asset "XLTCZEUR")
+       :sort [{:time {:order "asc"}}]
+       :size 1)
+
+;; count XLTCZEUR ticks:
+(ticks :query (filtered-query :query (match-query :asset "XLTCZEUR")
+                              :filter (daterange-filter :time (tcore/ago (tcore/days 1))))
+       :search_type "count")
+
+;; date hostogram over :last-price field of XLTCZEUR ticks:
+(ticks :query (filtered-query :query (match-query :asset "XLTCZEUR")
+                              :filter (daterange-filter :time (tcore/ago (tcore/hours 1))))
+       :facets {:price-last-hour
+                {:date_histogram {:key_field :time
+                                  :value_field :last-price
+                                  :interval "5m"}}}
+       :search_type "count")
