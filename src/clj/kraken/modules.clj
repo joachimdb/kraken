8367118ea09442af9e81276caed1251d734f65fc
Joachim De Beule <joachim.de.beule@gmail.com>
2014-02-20 02:13:57 +0100

    obsolete

--- src/clj/kraken/modules.clj
+++
@@ -1,8 +0,0 @@
-(ns kraken.modules
-  (:use [kraken.channels])
-  (:require [clojure.core.async :as as]
-            [kraken.elastic :as es]))
-
-(defn monitor [channel]
-  (as/admix (print-sink) channel))
-
